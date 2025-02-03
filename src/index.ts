ef3bce8a9e85fcfdbbfbe8823f8ebda791677b03
lucasgianine <lucas.esilva@sptech.school>
2025-02-03 14:26:03 -0300

    Obtém todas as sessões

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,53 @@
 import { prisma, findAnalysis, generateAnalysis } from './libs'
 
 ; (async function verifyAnalysis () {
-    const chooseSession = 16
     try {
-        const analysisFound = await findAnalysis(chooseSession)
+        const sessions = await prisma.session.findMany({ select: { id: true } })
 
-        if (!analysisFound) {
-            console.log('Não há analise para essa sessão, criando uma nova...')
+        for (const session of sessions) {
+            const sessionId = session.id
+            const analysisFound = await findAnalysis(sessionId)
 
-            const messages = await prisma.message.findMany({
-                where: {
-                    session_id: chooseSession
-                },
-                select: {
-                    content: true,
-                    created_at: true,
-                    remote: true
-                }
-            })
+            if (!analysisFound) {
+                console.log('Não há analise para essa sessão, criando uma nova...')
 
-            const analysis = await generateAnalysis(chooseSession, messages)
+                const messages = await prisma.message.findMany({
+                    where: {
+                        session_id: sessionId
+                    },
+                    select: {
+                        content: true,
+                        created_at: true,
+                        remote: true
+                    }
+                })
 
-            await prisma.analysis.create({
-                data: {
-                    session_id: chooseSession,
-                    satisfaction: analysis.satisfaction,
-                    summary: analysis.summary,
-                    improvement: analysis.improvement,
-                    created_at: new Date()
-                }
-            })
+                const analysis = await generateAnalysis(sessionId, messages)
 
-            console.log(`Análise da sessão ${chooseSession} criada:`)
-            console.log(
-                `Satisfação: ${analysis.satisfaction}\n` +
-                `Resumo: ${analysis.summary}\n` +
-                `Melhoria: ${analysis.improvement}`
-            )
-        } else {
-            console.log(`Análise da sessão ${chooseSession}:`)
-            console.log(
-                `Satisfação: ${analysisFound[0].satisfaction}\n` +
-                `Resumo: ${analysisFound[0].summary}\n` +
-                `Melhoria: ${analysisFound[0].improvement}`
-            )
+                await prisma.analysis.create({
+                    data: {
+                        session_id: sessionId,
+                        satisfaction: analysis.satisfaction,
+                        summary: analysis.summary,
+                        improvement: analysis.improvement,
+                        created_at: new Date()
+                    }
+                })
+
+                console.log(`Análise da sessão ${sessionId} criada:`)
+                console.log(
+                    `Satisfação: ${analysis.satisfaction}\n` +
+                    `Resumo: ${analysis.summary}\n` +
+                    `Melhoria: ${analysis.improvement}`
+                )
+            } else {
+                console.log(`Análise da sessão ${sessionId}:`)
+                console.log(
+                    `Satisfação: ${analysisFound[0].satisfaction}\n` +
+                    `Resumo: ${analysisFound[0].summary}\n` +
+                    `Melhoria: ${analysisFound[0].improvement}`
+                )
+            }
         }
     } catch (error) {
         console.error(`Ocorreu um erro ao verificar análises: ${error}`)
